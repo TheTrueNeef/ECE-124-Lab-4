c78384a4d6d9ede750b7079e383816421e0de6b9
Afnan-Alam <afnan.alam1971@gmail.com>
2025-03-26 16:20:05 -0400

    Temporary comments for holding register file
    Might need to change the d <= (sreg OR din) AND (NOT(reset OR register_clr));

--- holding_register.vhd
+++ holding_register.vhd
@@ -1,24 +1,32 @@
+-- Afnan Alam and Adnan Eddeb, Lab 4 Report, Lab Section 201, Group 21
+
 library ieee;
 use ieee.std_logic_1164.all;
 
+-- Declaring the holding register entity 
 entity holding_register is port (
-	clk           : in std_logic;
-	reset         : in std_logic;
-	register_clr  : in std_logic;
-	din           : in std_logic;
-	dout          : out std_logic
+	clk           : in std_logic;		-- Clock Input
+	reset         : in std_logic;		-- Input Reset Signal
+	register_clr  : in std_logic;		-- Input for the Reset Clear
+	din           : in std_logic;		-- Data Input
+	dout          : out std_logic		-- Data Output
 );
 end holding_register;
 
+-- Logic of the holding register 
 architecture circuit of holding_register is
+	-- defining signals to hold register 
 	signal sreg : std_logic;
+	-- storing a temporary variable to simplify logic in the if statement
 	signal d    : std_logic;
 begin
 	d <= (sreg OR din) AND (NOT(reset OR register_clr));
 
 	synced_system : process(clk)
 	begin
+		-- only proceeds if the clock is on the rising edge
 		if (rising_edge(clk)) then
+			-- Stores d in the register and outputs it in data output
 			sreg <= d;
 			dout <= d;
 		end if;
